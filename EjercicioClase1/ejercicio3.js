0909b880baccc9cfd57ea030d04a6ef2d59d1bad
DavidCamposH <Dc040105@gmail.com>
2024-05-15 13:45:33 -0600

    Update ejercicio3.js

--- EjercicioClase1/ejercicio3.js
+++ EjercicioClase1/ejercicio3.js
@@ -1,1 +1,11 @@
+//ejercicio 3
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Ingresa una cadena de texto: ', (cadena) => {
+  console.log(La cadena en mayúsculas es: ${cadena.toUpperCase()});
+  rl.close();
+});
