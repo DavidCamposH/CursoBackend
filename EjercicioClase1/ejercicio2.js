440859935e0fae91d651cf12c4373220cc91e458
DavidCamposH <Dc040105@gmail.com>
2024-05-15 13:44:18 -0600

    Update ejercicio2.js

--- EjercicioClase1/ejercicio2.js
+++ EjercicioClase1/ejercicio2.js
@@ -1,13 +1,10 @@
-//ejercicio 2
+//ejercicio2
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-
-// Programa 2: Contar caracteres
 rl.question('Ingresa una cadena de texto: ', (cadena) => {
   console.log(La longitud de la cadena es: ${cadena.length});
   rl.close();
