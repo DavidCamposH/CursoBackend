b61925830c0733ae6dcb7738992e15b14a9a12eb
DavidCamposH <Dc040105@gmail.com>
2024-05-15 13:40:58 -0600

    Update ejercicio1.js

--- EjercicioClase1/ejercicio1.js
+++ EjercicioClase1/ejercicio1.js
@@ -1,20 +1,14 @@
-//Determinar si un número ingresado por el usuario es positivo, negativo o si es cero
-
-const readline = require ('readline');
+const readline = require('readline');
 
 const rl = readline.createInterface({
-
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question("ingrese un numero: " , (numero) => {
-
-    if (numero >= 1 ){
-       return console.log(`el numero ${numero} es positivo` )
-    } else if (numero <= -1){
-       return  console.log(`el numero ${numero} negativo` )
-    }else {
-         return console.log ("el numero es 0")
-    }
+// Programa 1: Saludo con nombre y apellido
+rl.question('Ingresa tu nombre: ', (nombre) => {
+  rl.question('Ingresa tu apellido: ', (apellido) => {
+    console.log(Hola, ${nombre} ${apellido}!);
+    rl.close();
+  });
 });
