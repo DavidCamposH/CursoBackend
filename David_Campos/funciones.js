678fad1a82c2d52d428103e0a321979b4ca2ec1c
DavidCamposH <Dc040105@gmail.com>
2024-05-21 16:26:55 -0600

    Add files via upload

---
+++ David_Campos/funciones.js
@@ -0,0 +1,14 @@
+
+export function division(a, b) {
+ 
+    return a / b;
+}
+
+export function multiplicacion(a, b) {
+ 
+    return a * b;
+}
+
+export function raizCuadrada(a) {
+    return Math.sqrt(a);
+}
---
+++ David_Campos/index.js
@@ -0,0 +1,20 @@
+
+import { division } from './funciones.js'
+
+const resultadoDivision = division(10, 2)
+
+console.log(`El resultado de la división es ${resultadoDivision}`)
+
+import { multiplicacion} from './funciones.js'
+
+const resultadoMultiplicacion = multiplicacion(45,9)
+console.log(`El resultado de la multiplicacion es ${resultadoMultiplicacion}`)
+
+import { raizCuadrada } from './funciones.js';
+const resultadoRaizCuadrada = raizCuadrada();
+console.log(`La raíz cuadrada es ${resultadoRaizCuadrada}`)
+
+
+
+
+
